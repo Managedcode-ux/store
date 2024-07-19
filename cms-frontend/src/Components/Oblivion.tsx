effead93809718a9f75db83193a89202cc159ccf
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2024-07-19 20:31:51 +0530

    Image Carousel operations developed
    The operations panel that can be used to manipulate images is created. Functionality is still not present as file upload and storage needs to be implemented first

--- cms-frontend/src/Components/Oblivion.tsx
+++ cms-frontend/src/Components/Oblivion.tsx
@@ -1,15 +1,19 @@
-import Carousel from '../Elememts/ImageCarousel'
-import styles from './Oblivious.module.css'
-import ProductCard from './ProductCard'
+import styles from "./Oblivious.module.css";
+import ProductCard from "./ProductCard";
+import Carousel from "../Elememts/ImageCarousel";
+import { ImageOperation } from "../Elememts/ImageOperationButtons";
+import ImageUploader from "../Elememts/ImageUpload";
 
-export default function Oblivion(){
-    return(
-        <>
-        <div className={styles.workSpace}>
-            <p>Welcome to the oblivion</p>
-            {/* <ProductCard/> */}
-            <Carousel/>
-        </div>
-        </>
-    )
+export default function Oblivion() {
+  return (
+    <>
+      <div className={styles.workSpace}>
+        <p>Welcome to the oblivion</p>
+        {/* <ProductCard/> */}
+        {/* <Carousel /> */}
+        {/* <ImageOperation /> */}
+        <ImageUploader />
+      </div>
+    </>
+  );
 }
---
+++ cms-frontend/src/lib/appwrite.ts
@@ -0,0 +1,7 @@
+import { Client, Account } from "appwrite";
+
+export const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("669a693b0018db205477");
